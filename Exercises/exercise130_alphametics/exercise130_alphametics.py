8c17d81c6267a8d05078475612f0ff6126f4dcf3
Dusan Vilicic Held <dvheld@gmail.com>
2025-02-23 23:48:18 -0300

    Created solve stub and verify, split helper functions

--- Exercises/exercise130_alphametics/exercise130_alphametics.py
+++ Exercises/exercise130_alphametics/exercise130_alphametics.py
@@ -1,2 +1,40 @@
 """Alphametics exercise"""
 
+"""Alphametics exercise"""
+
+from string import ascii_uppercase
+
+def _split(puzzle: str, /) -> tuple:
+    addends, result = puzzle.replace(" ", "").split("==")
+    addends = addends.split("+")
+    return (addends, result)
+
+def _validate(puzzle: any, /) -> None:
+    if not puzzle:
+        raise ValueError("Missing input.")
+    if not isinstance(puzzle, str):
+        raise TypeError("Input must be a string.")
+    if len(set(puzzle.replace(" ", "").replace("+", "").replace("=" , ""))) > 10:
+        raise ValueError("Too many different letters. Maximum is 10.")
+    for char in puzzle:
+        if char not in ascii_uppercase + "= +":
+            raise ValueError(f"Invalid character in puzzle: '{char}'")
+
+def solve(puzzle: str, /) -> dict:
+    """Solves the provided alphametics puzzle.
+
+    :param str puzzle: The puzzle.
+    :return dict: The solution.
+    """
+
+    _validate(puzzle)
+    addends, result = _split(puzzle)
+    solution = {}
+
+    return solution
+
+######################### TESTING AREA #########################
+
+mypuzzle = "SEND + MORE == MONEY"
+
+# print(_validate(mypuzzle))
