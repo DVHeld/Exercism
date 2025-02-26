38da3d6a9f126f6278c0f6b863439849e571b2e0
Dusan Vilicic Held <dvheld@gmail.com>
2025-02-26 10:51:31 -0300

    Created _extract_operations helper function
    It creates a list of lists of strings that represent the sub-operations implicit in the puzzle's sum. I.e. "SEND + MORE = MONEY" yields the operations D+E=Y, N+R=E, E+O=N, S+M=O and _=M. Finally these operations are represented in the list as follows: [['DE', 'Y'], ['NR', 'E'], ['EO', 'N'], ['SM', 'O'], ['', 'M']. Remainders are accounted for in a later calculation.

--- Exercises/exercise130_alphametics/exercise130_alphametics.py
+++ Exercises/exercise130_alphametics/exercise130_alphametics.py
@@ -3,11 +3,6 @@
 from string import ascii_uppercase
 from collections import defaultdict
 
-def _split(puzzle: str, /) -> tuple:
-    addends, result = puzzle.replace(" ", "").split("==")
-    addends = addends.split("+")
-    return (addends, result)
-
 def _validate(puzzle: any, /) -> None:
     if not puzzle:
         raise ValueError("Missing input.")
@@ -19,6 +14,18 @@ def _validate(puzzle: any, /) -> None:
         if char not in ascii_uppercase + "= +":
             raise ValueError(f"Invalid character in puzzle: '{char}'")
 
+def _split(puzzle: str, /) -> tuple:
+    addends, result = puzzle.replace(" ", "").split("==")
+    addends = addends.split("+")
+    return (addends, result)
+
+def _extract_operations(addends: list, result: str, /) -> list:
+    operations = [([], letter) for letter in reversed(result)]
+    for addend in addends:
+        for index, letter in enumerate(reversed(addend)):
+            operations[index][0].append(letter)
+    return operations
+
 def solve(puzzle: str, /) -> dict:
     """Solves the provided alphametics puzzle.
 
@@ -29,11 +36,13 @@ def solve(puzzle: str, /) -> dict:
     _validate(puzzle)
 
     addends, result = _split(puzzle)
-    solution = defaultdict(int)
-    max_len = max(max(len(addend) for addend in addends), len(solution))
-    remainders = [0] * max_len
+    operations = _extract_operations(addends, result)
+    max_len = max(max(len(addend) for addend in addends), len(result))
     letters = set(''.join(result).join(addends))
+    remainders = [0] * max_len
     letter_digits = {letter:list(range(10)) for letter in letters}
+    solution = defaultdict(int)
+
 
     column = max_len
     while column > 0:
